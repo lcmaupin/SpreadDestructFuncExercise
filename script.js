d44cb4036c8a10a59551010348c5ee71606f1b6d
Luke Maupin <lukecmaupin@gmail.com>
2021-10-20 20:29:10 -0400

    Adds bonus answers

--- script.js
+++ script.js
@@ -23,3 +23,29 @@ let seeCharacters = ({ellieSattler}) => {
 }
 seeCharacters(jPCharacters);
 
+// 3a
+const jurassicParkMovies = [
+    {
+        one: `Jurassic Park`,
+        two: `The Lost World: Jurassic Park`,
+        three: `Jurassic Park III`
+    },
+    {
+        four: `Jurassic World`,
+        five: `Jurassic World: Fallen Kingdom`,
+        six: `Jurassic World: Dominion`
+    }
+];
+
+console.log("==========");
+
+// 3b
+function seeJPMovies([{one, two, three}, {four, five, six}]){
+    console.log(one);
+    console.log(two);
+    console.log(three);
+    console.log(four);
+    console.log(five);
+    console.log(six);
+}
+seeJPMovies(jurassicParkMovies);
